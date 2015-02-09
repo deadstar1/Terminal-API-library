6a4b02c1bd44a3c8e1ec78d5a88f82d8aee51e48
mar adrian belen <mardrianbelen@gmail.com>
2015-02-09 21:50:24 +0800

    Update curl.php

--- lib/curl.php
+++ lib/curl.php
@@ -2,22 +2,23 @@
 class curl{
 
 	function curl_terminal($endpoint,$param){
-		echo $endpoint;
+		//echo $endpoint;
 
 		$curl=curl_init();
 		$param=json_encode($param);
-		echo $param;
+		//echo $param;
 		$endpoint='https://api.terminal.com/v0.1'. $endpoint;
 
-		curl_setopt($curl,CURLOPT_URL, $endpoint);
-		curl_setopt($curl,CURL_HTTPHEADER, array('Content-Type: application/json'));
-		curl_setopt($curl,CURL_POST, 1);
-		curl_setopt($curl,CURL_POSTFIELDS, $param);
-		curl_setopt($curl,CURL_RETURNTRANSFER, 1);
-		curl_setopt($curl,CURL_HTTPAUTH, CURLAUTH_ANY);
-		curl_setopt($curl,CURL_FOLLOWLOCATION, 1);
-		curl_setopt($curl,CURL_SSL_VERFIYPEER, false);
+		curl_setopt($curl, CURLOPT_URL, $endpoint);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+		curl_setopt($curl, CURLOPT_POST, 1);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $param);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
+		curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 		$RESULT=curl_exec($curl);
+		var_dump($RESULT);
 
 	}
 
