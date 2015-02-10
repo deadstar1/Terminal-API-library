36de704bd76c5a23de250d2fd50b5279fb498887
mar adrian belen <mardrianbelen@gmail.com>
2015-02-10 16:54:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include_once('/lib/endpoints.php');
 $terminal = new endpoint;
 $terminal -> user_token='';
 $terminal -> access_token='';
- $test = ($terminal -> get_snapshot('166bff1e4873c3d75cfd3d3dabbeeedecf8926f84174b6d0fda5b081b948ac33'));
+ $test = ($terminal -> get_snapshot(''));
  var_dump($test);
 
 ?>
