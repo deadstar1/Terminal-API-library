9db107d8ce229865f970217c57e64cf8dfe26661
mar adrian belen <mardrianbelen@gmail.com>
2015-02-11 09:12:54 +0800

    Update index.php
    this is a example!

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?PHP
-include_once('/lib/endpoints.php');
-$terminal = new endpoint;
-$terminal -> user_token='';
-$terminal -> access_token='';
+include_once('/lib/endpoints.php'); 
+$terminal = new endpoint; //new instance of endpoint class
+$terminal -> user_token=''; // your user_token
+$terminal -> access_token='';//your access_token
  $test = ($terminal -> get_snapshot(''));
  var_dump($test);
 
